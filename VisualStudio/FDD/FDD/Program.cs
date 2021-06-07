1d8461a9c75cafbc87ce451319d595cafb16c232
KinGelaim <89123232156@mail.ru>
2021-06-07 21:25:27 +0500

    Add FDD

---
+++ VisualStudio/FDD/FDD/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+
+namespace FDD
+{
+    class Program
+    {
+        private static string path = @"A:\";
+
+        static void Main(string[] args)
+        {
+            string[] musics = { "Похоронка", "Имперский марш!", "Собачий вальс" };
+            Console.WriteLine("Добро пожаловать в бар Мюзикс!\n");
+            Console.Write("Какой режим Вы заказываете (beep/ffd): ");
+            string form = Console.ReadLine();
+            if (form != "beep" && form != "ffd")
+            {
+                Console.WriteLine("Упс!");
+            }
+            else
+            {
+                Console.WriteLine("Какую песню закажите:");
+                for (int i = 0; i < musics.Length; i++)
+                {
+                    Console.WriteLine((i + 1) + " - " + musics[i]);
+                }
+                string key = Console.ReadLine();
+                switch (key)
+                {
+                    case "1":
+                        Console.WriteLine("Проигрываем музыку похоронки");
+                        play(form);
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    default:
+                        Console.WriteLine("Увы, такой песни у нас нет");
+                        break;
+                }
+            }
+        }
+
+        private static void play(string form)
+        {
+            if (form == "beep")
+            {
+                Console.Beep(200, 900);
+                Console.Beep(200, 900);
+                Console.Beep(400, 600);
+                Console.Beep(170, 900);
+                Console.Beep(200, 900);
+                Console.Beep(400, 600);
+                Console.Beep(170, 900);
+                Console.Beep(400, 600);
+                Console.Beep(170, 900);
+                Console.Beep(400, 600);
+                Console.Beep(170, 900);
+            }
+            if (form == "ffd")
+            {
+                playSound(30, 200);
+                playSound(30, 200);
+                playSound(10, 100);
+                playSound(30, 200);
+                playSound(20, 200);
+                playSound(10, 100);
+                playSound(20, 200);
+                playSound(10, 100);
+                playSound(20, 200);
+                playSound(10, 100);
+                playSound(20, 200);
+            }
+        }
+
+        private static void playSound(int count, int delay = 0)
+        {
+            for (int i = 0; i < count; i++)
+                try
+                {
+                    using (FileStream fs = new FileStream(path, FileMode.Open))
+                    {
+
+                    }
+                }
+                catch (Exception e)
+                {
+                    //Console.WriteLine(e.Message);
+                }
+                finally
+                {
+
+                }
+            Thread.Sleep(delay);
+        }
+    }
+}
---
+++ VisualStudio/FDD/FDD/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("FDD")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FDD")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("e2b1178b-e61c-4623-89db-03b20642b60d")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
