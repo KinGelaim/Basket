42f8c7d2c78f7273a491eb39e836c2f1f91622ec
KinGelaim <89123232156@mail.ru>
2021-06-07 21:01:27 +0500

    ThreadTCP_Console

---
+++ VisualStudio/ThreadTCP_Console/Client/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+namespace Client
+{
+    class Program
+    {
+        static Thread threadInput;
+        static Thread threadSend;
+        static NetworkStream nwStream;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ip: ");
+            string ip = Console.ReadLine();
+            Console.WriteLine("Подключение к серверу...");
+            TcpClient client = new TcpClient(ip, 4000);
+            nwStream = client.GetStream();
+            Console.WriteLine("Подключение удалось!");
+            StreamReader stReader = new StreamReader(nwStream);
+            StreamWriter stWriter = new StreamWriter(nwStream) { AutoFlush = true };
+            new Thread(() => {
+                while (true)
+                {
+                    Console.WriteLine("Введите сообщение: ");
+                    string send = Console.ReadLine();
+                    stWriter.WriteLine(send);
+                    Console.WriteLine("Сообщение отправлено!");
+                }
+            }).Start();
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    string input = stReader.ReadLine();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("С сервера пришло: " + input);
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+            }).Start();
+        }
+
+        static void CurrentDomain_ProcessExit(object sender, EventArgs e)
+        {
+            threadInput.Start();
+            threadSend.Start();
+            if (threadInput.IsAlive)
+                threadSend.Abort();
+            if (threadSend.IsAlive)
+                threadSend.Abort();
+            nwStream.Close();
+        }
+    }
+}
---
+++ VisualStudio/ThreadTCP_Console/ThreadTCP_Console/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+namespace Server
+{
+    class Program
+    {
+        static Thread threadInput;
+        static Thread threadSend;
+        static TcpListener tcpListener;
+        static NetworkStream nwStream;
+
+        static void Main(string[] args)
+        {
+            AppDomain.CurrentDomain.ProcessExit += new EventHandler(ProcessExit);
+            tcpListener = new TcpListener(IPAddress.Any, 4000);
+            tcpListener.Start();
+            Console.WriteLine("Сервер запущен!");
+            TcpClient tcpClient = tcpListener.AcceptTcpClient();
+            nwStream = tcpClient.GetStream();
+            Console.WriteLine("Клиент подключен!");
+            StreamWriter stWriter = new StreamWriter(nwStream, Encoding.UTF8) {
+                AutoFlush = true
+            };
+            StreamReader stReader = new StreamReader(nwStream, Encoding.UTF8);
+            threadInput = new Thread(() =>
+            {
+                while (true)
+                {
+                    string input = stReader.ReadLine();
+                    Console.WriteLine("Клиент написал: " + input);
+                    stWriter.WriteLine("Сервер получил сообщение равное: " + input.Length);
+                }
+            });
+            threadSend = new Thread(() =>
+            {
+                while (true)
+                {
+                    string send = Console.ReadLine();
+                    stWriter.WriteLine(send);
+                }
+            });
+            threadInput.Start();
+            threadSend.Start();
+        }
+
+        static void ProcessExit(object sender, EventArgs e)
+        {
+            if (threadInput.IsAlive)
+                threadSend.Abort();
+            if (threadSend.IsAlive)
+                threadSend.Abort();
+            nwStream.Close();
+            tcpListener.Stop();
+        }
+    }
+}
