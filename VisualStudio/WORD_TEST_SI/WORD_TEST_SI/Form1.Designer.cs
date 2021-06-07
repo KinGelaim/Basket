e543c59b112d6016710d64f0ff43ca6093dbca56
KinGelaim <89123232156@mail.ru>
2021-06-07 21:24:23 +0500

    Add Word_Test_SI

---
+++ VisualStudio/WORD_TEST_SI/WORD_TEST_SI/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace WORD_TEST_SI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.Location = new System.Drawing.Point(77, 262);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 77);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Напечатать отчет";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(204, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите количество экземляров";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(94, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Введите учетный номер";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(67, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(201, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "2";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(97, 87);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(146, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "40";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(127, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Введите ГОД";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(97, 140);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(146, 20);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "2018";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(118, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Введите МЕСЯЦ";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(97, 190);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(146, 20);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.Text = "ЯНВАРЬ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 351);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Печать отчета";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ VisualStudio/WORD_TEST_SI/WORD_TEST_SI/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace WORD_TEST_SI
+{
+    public partial class Form1 : Form
+    {
+        private readonly string templateFileName = @"C:\Users\ad\Documents\Отчеты C#\ШАБЛОН СПРАВКА-ОБОСНОВАНИЯ.docx";
+        private DateTime dateNowFileName;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string countEkz = textBox1.Text;
+            string uchNumber = textBox2.Text;
+            string month = textBox4.Text;
+            string year = textBox3.Text;
+            Word.Application oWord = new Word.Application();
+            oWord.Visible = true;
+            try
+            {
+                //Создание нового документа
+                /*Object template = Type.Missing;
+                Object newTemplate = false;
+                Object documentType = Word.WdNewDocumentType.wdNewBlankDocument;
+                Object visible = true;
+                oWord.Documents.Add(ref template, ref newTemplate, ref documentType, ref visible);
+                Word.Document oDoc = (Word.Document)oWord.Documents.get_Item(1);
+                //Поля документа
+                oDoc.PageSetup.LeftMargin = oWord.CentimetersToPoints((float)1.0);
+                oDoc.PageSetup.TopMargin = oWord.CentimetersToPoints((float)1.0);
+                oDoc.PageSetup.RightMargin = oWord.CentimetersToPoints((float)1.0);
+                oDoc.PageSetup.BottomMargin = oWord.CentimetersToPoints((float)1.0);
+                //Альбомная ориентация
+                oDoc.PageSetup.Orientation = Word.WdOrientation.wdOrientLandscape;*/
+                //Открытие документа
+                Word.Document wordDocument = oWord.Documents.Open(templateFileName);
+                //Поиск и замена текста
+                functionReplaceInText(wordDocument, "{numberEkz}", countEkz);
+                functionReplaceInText(wordDocument, "{uchNumber}", uchNumber);
+                functionReplaceInText(wordDocument, "{month}", month);
+                functionReplaceInText(wordDocument, "{year}", year);
+                //Добавление в файл таблицы
+                wordDocument.Paragraphs.Add();
+                Object autoFitBehavior = Word.WdAutoFitBehavior.wdAutoFitWindow;
+                var range = wordDocument.Paragraphs[wordDocument.Paragraphs.Count - 1].Range;
+                wordDocument.Tables.Add(range, 1, 5, autoFitBehavior);
+                Word.Table oTable = wordDocument.Tables[1];
+                string[] nameHeaderTable = {"Код комплектующего элемента","Ед. измерения","Потребность на месяц","Наличие на складе","Испытуемый элемент","Код испытуемого элемента","Завод изготовитель","Объем испытаний"};
+                for (int i = 0; i < oTable.Columns.Count; i++)
+                {
+                    Word.Range wordCellRange = oTable.Cell(1, i + 1).Range;
+                    wordCellRange.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                    wordCellRange.Text = nameHeaderTable[i];
+                }
+                oTable.Cell(1, 5).Split(2, 1);
+                for (int i = 2; i < oTable.Rows.Count + 1; i++)
+                {
+                    oTable.Cell(i, 5).Split(1, 3);
+                }
+                for (int i = 5; i < 8; i++)
+                {
+                    Word.Range wordCellRange = oTable.Cell(2, i).Range;
+                    wordCellRange.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                    wordCellRange.Text = nameHeaderTable[i];
+                }
+                int countKompEl = 4;
+                int countIspEl = 2;
+                for (int i = 0; i < countKompEl; i++)
+                {
+                    oTable.Rows.Add();
+                    for (int j = 0; j < 4; j++)
+                    {
+                        Word.Range wordCellRange = oTable.Cell(i + 3, j + 1).Range;
+                        wordCellRange.Text = "sd";
+                    }
+                    for (int k = 1; k < countIspEl; k++)
+                    {
+                        for (int j = 5; j < 8; j++)
+                        {
+                            Word.Range wordCellRange = oTable.Cell(i + 3, j).Range;
+                            wordCellRange.Text = "5";
+                        }
+                        oTable.Cell(i + 3, 5).Split(2, 1);
+                        oTable.Cell(i + 3, 6).Split(2, 1);
+                        oTable.Cell(i + 3, 7).Split(2, 1);
+                        i++;
+                        countKompEl++;
+                    }
+                    for (int j = 5; j < 8; j++)
+                    {
+                        Word.Range wordCellRange = oTable.Cell(i + 3, j).Range;
+                        wordCellRange.Text = "4";
+                    }
+                }
+                //Сохранение
+                dateNowFileName = DateTime.Now;
+                string strSaveName = Convert.ToString(dateNowFileName);
+                strSaveName = strSaveName.Replace(':', '-');
+                wordDocument.SaveAs(@"C:\Users\ad\Documents\Отчеты C#\" + strSaveName + ".docx");
+                wordDocument.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка!");
+            }
+            finally
+            {
+                oWord.Quit();
+                this.Close();
+            }
+        }
+
+        private void functionReplaceInText(Word.Document wordDocument, string findText, string replaceText)
+        {
+            var range = wordDocument.Content;
+            range.Find.ClearFormatting();
+            range.Find.Execute(FindText: findText, ReplaceWith: replaceText);
+        }
+    }
+}
