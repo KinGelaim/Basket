f4a31bc11f2d395ebeb8d5da63d5fdadf91c2cf3
KinGelaim <89123232156@mail.ru>
2021-06-07 21:02:04 +0500

    Add ThreadTCP_Forms

---
+++ VisualStudio/ThreadTCP_Forms/ThreadTCP_Forms/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace ThreadTCP_Forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите IP";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(19, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(214, 29);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "127.0.0.1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Введите порт";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(19, 111);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            90000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(214, 29);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            4000,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(354, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(274, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Введите порт для прослушки";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(358, 36);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            90000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(270, 29);
+            this.numericUpDown2.TabIndex = 5;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            4000,
+            0,
+            0,
+            0});
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(358, 146);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(270, 36);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Создать сервер";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(19, 146);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(220, 36);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Подключиться";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Location = new System.Drawing.Point(19, 217);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(609, 29);
+            this.textBox2.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(358, 252);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(270, 36);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Отправить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 303);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ThreadTCP";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ VisualStudio/ThreadTCP_Forms/ThreadTCP_Forms/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+namespace ThreadTCP_Forms
+{
+    public partial class Form1 : Form
+    {
+        static Thread threadInput;
+        static Thread threadSend;
+        static TcpListener tcpListener;
+        static NetworkStream nwStream;
+        static StreamWriter stWriter;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Создать сервер
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tcpListener = new TcpListener(IPAddress.Any, Convert.ToInt32(numericUpDown2.Value));
+            tcpListener.Start();
+            Console.WriteLine("Сервер запущен!");
+            TcpClient tcpClient = tcpListener.AcceptTcpClient();
+            nwStream = tcpClient.GetStream();
+            Console.WriteLine("Клиент подключен!");
+            stWriter = new StreamWriter(nwStream, Encoding.UTF8)
+            {
+                AutoFlush = true
+            };
+            StreamReader stReader = new StreamReader(nwStream, Encoding.UTF8);
+            threadInput = new Thread(() =>
+            {
+                while (true)
+                {
+                    try
+                    {
+                        string input = stReader.ReadLine();
+                        MessageBox.Show("Клиент написал: " + input);
+                    }
+                    catch (System.IO.IOException ex)
+                    {
+                        MessageBox.Show("Ошибка при получении данных от клиента!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        break;
+                    }
+                }
+            });
+            threadInput.Start();
+        }
+
+        //Подключиться
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Подключение к серверу...");
+            try
+            {
+                int k = 5 / Convert.ToInt32(numericUpDown1.Value);
+                TcpClient client = new TcpClient(textBox1.Text, Convert.ToInt32(numericUpDown1.Value));
+                nwStream = client.GetStream();
+                Console.WriteLine("Подключение удалось!");
+                StreamReader stReader = new StreamReader(nwStream);
+                stWriter = new StreamWriter(nwStream) { AutoFlush = true };
+                new Thread(() =>
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            string input = stReader.ReadLine();
+                            MessageBox.Show("С сервера пришло: " + input);
+                        }
+                        catch (System.IO.IOException ex)
+                        {
+                            MessageBox.Show("Ошибка при получении данных от сервера!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            break;
+                        }
+                    }
+                }).Start();
+            }
+            catch (System.Net.Sockets.SocketException ex)
+            {
+                MessageBox.Show("Не удалось установить соединение, повторите попытку позже!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        //Отправка сообщения
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text.Length > 0)
+            {
+                stWriter.WriteLine(textBox2.Text);
+                textBox2.Text = "";
+            }
+        }
+    }
+}
