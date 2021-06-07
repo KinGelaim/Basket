87b343a35d8085150a9455775805cfc5e3533f7e
KinGelaim <89123232156@mail.ru>
2021-06-07 21:36:21 +0500

    Add Mat

---
+++ VisualStudio/Mat/Mat/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace Mat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(521, 484);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 484);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Коврик";
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ VisualStudio/Mat/Mat/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Mat
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bitmap;
+        Graphics g;
+        int red = 0, green = 0, blue = 0;
+        bool isResize = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+            bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bitmap);
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (isResize)
+            {
+                //Находим длину одного сектора
+                double lenOneSector = Convert.ToDouble(pictureBox1.Width) / 4;
+                //Находим коэфицент покрытия
+                double koef = 255 / lenOneSector;
+                red = 0;
+                green = 0;
+                blue = 0;
+                for (int i = 0; i < pictureBox1.Width; i++)
+                {
+                    if (red == 0 && green == 0 && blue < 255)
+                        blue += Convert.ToInt32(Math.Ceiling(koef));
+                    if (red == 0 && green < 255 && blue >= 255)
+                        green += Convert.ToInt32(Math.Ceiling(koef));
+                    if (red == 0 && green >= 255 && blue > 0)
+                        blue -= Convert.ToInt32(Math.Ceiling(koef));
+                    if (red < 255 && green >= 255 && blue <= 0)
+                        red += Convert.ToInt32(Math.Ceiling(koef));
+                    ProverkaGranic();
+                    int x = i, y = 0;
+                    g.FillRectangle(new SolidBrush(Color.FromArgb(red, green, blue)), x, y, Convert.ToInt32(Math.Ceiling(koef)), Convert.ToInt32(Math.Ceiling(koef)));
+                    while (x - Convert.ToInt32(Math.Ceiling(koef)) >= 0 && y + Convert.ToInt32(Math.Ceiling(koef)) <= pictureBox1.Height)
+                    {
+                        x -= Convert.ToInt32(Math.Ceiling(koef));
+                        y += Convert.ToInt32(Math.Ceiling(koef));
+                        g.FillRectangle(new SolidBrush(Color.FromArgb(red, green, blue)), x, y, Convert.ToInt32(Math.Ceiling(koef)), Convert.ToInt32(Math.Ceiling(koef)));
+                    }
+                }
+                //Находим длину одного сектора (уже по высоте)
+                lenOneSector = Convert.ToDouble(pictureBox1.Height) / 4;
+                //Находим коэфицент покрытия
+                koef = 255 / lenOneSector;
+                for (int i = 0; i < pictureBox1.Height; i++)
+                {
+                    if (red >= 255 && green >= 255 && blue < 255)
+                        blue += Convert.ToInt32(Math.Ceiling(koef));
+                    if (red >= 255 && green > 0 && blue >= 255)
+                        green -= Convert.ToInt32(Math.Ceiling(koef));
+                    if (red >= 255 && green <= 0 && blue > 0)
+                        blue -= Convert.ToInt32(Math.Ceiling(koef));
+                    if (red > 0 && green <= 0 && blue <= 0)
+                        red -= Convert.ToInt32(Math.Ceiling(koef));
+                    ProverkaGranic();
+                    int x = pictureBox1.Width - Convert.ToInt32(Math.Ceiling(koef));
+                    int y = i;
+                    g.FillRectangle(new SolidBrush(Color.FromArgb(red, green, blue)), x, y, Convert.ToInt32(Math.Ceiling(koef)), Convert.ToInt32(Math.Ceiling(koef)));
+                    while (x - Convert.ToInt32(Math.Ceiling(koef)) >= 0 && y - Convert.ToInt32(Math.Ceiling(koef)) >= 0)
+                    {
+                        x -= Convert.ToInt32(Math.Ceiling(koef));
+                        y += Convert.ToInt32(Math.Ceiling(koef));
+                        g.FillRectangle(new SolidBrush(Color.FromArgb(red, green, blue)), x, y, Convert.ToInt32(Math.Ceiling(koef)), Convert.ToInt32(Math.Ceiling(koef)));
+                    }
+                }
+                pictureBox1.Image = bitmap;
+                isResize = false;
+            }
+        }
+
+        private void ProverkaGranic()
+        {
+            if (red > 255)
+                red = 255;
+            if (blue > 255)
+                blue = 255;
+            if (green > 255)
+                green = 255;
+            if (red < 0)
+                red = 0;
+            if (blue < 0)
+                blue = 0;
+            if (green < 0)
+                green = 0;
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bitmap);
+            isResize = true;
+        }
+    }
+}
