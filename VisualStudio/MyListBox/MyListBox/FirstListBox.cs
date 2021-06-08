f1706a8c0a6ece1b0dc7c73b0e9a4a45e58246ad
KinGelaim <89123232156@mail.ru>
2021-06-08 21:18:05 +0500

    Add MyListBox

---
+++ VisualStudio/MyListBox/MyListBox/FirstListBox.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MyListBox
+{
+    public class FirstListBox : ListBox
+    {
+        const int cornerRadius = 5; //Радиус скругления
+        int x, y, rectWidth, rectHeight;    //Переменные для отрисовки
+
+        public FirstListBox()
+        {
+            DrawMode = DrawMode.OwnerDrawVariable;
+        }
+
+        protected override void OnDrawItem(DrawItemEventArgs e)
+        {
+            //e - элемент с которым продолжается работа
+            //Если элемента нет или список пуст - выходим
+            if (e.Index <= -1 || this.Items.Count == 0) return;
+
+            string s = Items[e.Index].ToString();   //Текст элемента
+
+            //Формат строки для рисования
+            StringFormat sf = new StringFormat();
+            sf.Alignment = StringAlignment.Near;
+
+            Brush solidBrush = new SolidBrush(Color.FromArgb(45, 131, 218));
+            Brush gradientBrush = new LinearGradientBrush(e.Bounds, Color.FromArgb(221, 30, 207), Color.FromArgb(65, 151, 238), LinearGradientMode.Vertical);
+
+            //Отрисовываем не активный элемент
+            if ((e.State & DrawItemState.Focus) == 0)
+            {
+                e.Graphics.FillRectangle(new SolidBrush(SystemColors.Window), e.Bounds.X, e.Bounds.Y, e.Bounds.Width, e.Bounds.Height + 1);
+                e.Graphics.DrawString(s, Font, new SolidBrush(SystemColors.WindowText), new RectangleF(0, e.Bounds.Y, e.Bounds.Width, 40), sf);
+            }
+            else
+            {
+                GraphicsPath gfxPath = new GraphicsPath();
+                x = e.Bounds.X;
+                y = e.Bounds.Y;
+                rectWidth = e.Bounds.Width - 2;
+                rectHeight = e.Bounds.Height;
+
+                #region Рисуем прямоугольник с закругленными углами
+                gfxPath.AddLine(x + cornerRadius, y, x + rectWidth - (cornerRadius * 2), y);
+                gfxPath.AddArc(x + rectWidth - (cornerRadius * 2), y, cornerRadius * 2, cornerRadius * 2, 270, 90);
+                gfxPath.AddLine(x + rectWidth, y + cornerRadius, x + rectWidth, y + rectHeight - (cornerRadius * 2));
+                gfxPath.AddArc(x + rectWidth - (cornerRadius * 2), y + rectHeight - (cornerRadius * 2), cornerRadius * 2, cornerRadius * 2, 0, 90);
+                gfxPath.AddLine(x + rectWidth - (cornerRadius * 2), y + rectHeight, x + cornerRadius, y + rectHeight);
+                gfxPath.AddArc(x, y + rectHeight - (cornerRadius * 2), cornerRadius * 2, cornerRadius * 2, 90, 90);
+                gfxPath.AddLine(x, y + rectHeight - (cornerRadius * 2), x, y + cornerRadius);
+                gfxPath.AddArc(x, y, cornerRadius * 2, cornerRadius * 2, 180, 90);
+                gfxPath.CloseFigure();
+                e.Graphics.DrawPath(new Pen(solidBrush, 1), gfxPath);
+
+                //Закраска области
+                e.Graphics.FillPath(gradientBrush, gfxPath);
+                gfxPath.Dispose();
+                #endregion
+
+                e.Graphics.DrawString(s, Font, new SolidBrush(Color.White), new RectangleF(0, e.Bounds.Y, e.Bounds.Width, 40), sf);
+            }
+        }
+
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            Refresh();
+            base.OnSizeChanged(e);
+        }
+
+        protected override void OnMeasureItem(MeasureItemEventArgs e)
+        {
+            if (e.Index > -1)
+            {
+                e.ItemHeight = 40;
+                e.ItemWidth = Width;
+            }
+        }
+    }
+}
---
+++ VisualStudio/MyListBox/MyListBox/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace MyListBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstListBox1 = new MyListBox.FirstListBox();
+            this.SuspendLayout();
+            // 
+            // firstListBox1
+            // 
+            this.firstListBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.firstListBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            this.firstListBox1.FormattingEnabled = true;
+            this.firstListBox1.Items.AddRange(new object[] {
+            "1142314234 1234172835678123 ghrgwjkegjh23gkj41g2jk3 g4h12gk3 4j12gh3k j412g3uj4 1" +
+                "2jkg4j12",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9"});
+            this.firstListBox1.Location = new System.Drawing.Point(12, 12);
+            this.firstListBox1.Name = "firstListBox1";
+            this.firstListBox1.Size = new System.Drawing.Size(260, 233);
+            this.firstListBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.firstListBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private FirstListBox firstListBox1;
+
+    }
+}
+
