9ae3125543f89ff15bf548201307a82f12a716d1
KinGelaim <47106074+KinGelaim@users.noreply.github.com>
2021-06-09 20:20:35 +0500

    Drop Django
    Django messes up my GIT stats.

--- WEB/django/Django-3.0.10/blog/blog/apps/articles/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-app_name = 'articles'
-urlpatterns = [
-    url(r'^(\d+)/$', views.detail, name = 'detail'),
-    url(r'^(\d+)/add_comment/$', views.add_comment, name = 'add_comment'),
-	url('', views.index, name = 'index'),
-]
