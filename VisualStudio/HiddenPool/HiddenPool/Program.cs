414447fab3d5af13cde1aa780829344f73c34cfa
KinGelaim <89123232156@mail.ru>
2022-03-21 19:35:12 +0500

    HiddenPool

---
+++ VisualStudio/HiddenPool/HiddenPool/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace HiddenPool
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            ArglistAndParams();
+
+            RefKeywords();
+            RefKeywordsUnsafe();
+        }
+
+        #region arglist
+
+        private static void ArglistAndParams()
+        {
+            WithArglist(__arglist(2, 3, true, "string"));
+            WithParams(2, 3, true, "string");
+        }        
+
+        // __arglist - указатель, где лежат параметры
+        private static void WithArglist(__arglist)
+        {
+            //Console.WriteLine(nameof(WithArglist));
+
+            var argIterator = new ArgIterator(__arglist);
+            DumpArgs(argIterator);
+
+            Console.WriteLine();
+        }
+
+        private static void DumpArgs(ArgIterator args)
+        {
+            while(args.GetRemainingCount() > 0)
+            {
+                TypedReference tr = args.GetNextArg();
+                var obj = TypedReference.ToObject(tr);
+                var type = TypedReference.GetTargetType(tr);
+                Console.WriteLine(obj  + " / " + type);
+            }
+        }
+
+        // params - массив!!!
+        private static void WithParams(params object[] args)
+        {
+            //Console.WriteLine(nameof(WithParams));
+            foreach (var arg in args)
+            {
+                Console.WriteLine(arg + " / " + arg.GetType());
+            }
+            Console.WriteLine();
+        }
+
+        #endregion
+
+        #region makeref
+
+        private static void RefKeywords()
+        {
+            double value = 10;
+            TypedReference tr = __makeref(value);   // tr = &value;
+
+            Console.WriteLine( __refvalue(tr, double));
+
+            __refvalue(tr, double) = 11;    // *tr = 11;
+            Console.WriteLine( __refvalue(tr, double));
+
+            Type type = __reftype(tr);  // value.GetType();
+            Console.WriteLine(type.Name);
+
+            Console.WriteLine();
+        }
+
+        unsafe private static void RefKeywordsUnsafe()
+        {
+            double value = 10;
+            double* tr = &value;
+
+            Console.WriteLine(*tr);
+
+            *tr = 11;
+            Console.WriteLine(*tr);
+
+            Console.WriteLine();
+
+            Console.WriteLine();
+        }
+
+        #endregion
+
+    }
+}
---
+++ VisualStudio/HiddenPool/HiddenPool/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("HiddenPool")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HiddenPool")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("530d7355-57d0-4331-8832-c7d161583f18")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
